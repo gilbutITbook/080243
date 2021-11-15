4426776117f0a9ffe47dd551f1b76b6be80570a1
길벗 IT전문서 <6995518+gilbutITbook@users.noreply.github.com>
2021-11-15 11:22:32 +0900

    Add files via upload

---
+++ sed/comment-test.c
@@ -0,0 +1,12 @@
+first /*** inside */ 2 /* second ***/ third
+first /*** inside */ 2 /* second ***/ /*
+lllllllllllllllllllllllll
+                                  */
+extra
+/* what */ here
+first /*** inside */ 2 // /* second ***/ third
+first /*** inside */ 2 // second ***/ /* <- comment start within
+lllllllllllllllllllllllll
+"this is stray, and should remain so" */
+extra
+/* what */ here
